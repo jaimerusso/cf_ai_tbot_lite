80dac7fcd1849985497fb06f2bc1e23d840cce9d
Jaime Russo <itsjaimerusso@gmail.com>
2026-04-14 15:02:56 +0100

    front and back base structure scribble

---
+++ backend/src/index.ts
@@ -0,0 +1,75 @@
+import { DurableObject } from "cloudflare:workers";
+
+/**
+ * Welcome to Cloudflare Workers! This is your first Durable Objects application.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your Durable Object in action
+ * - Run `npm run deploy` to publish your application
+ *
+ * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
+ * `Env` object can be regenerated with `npm run cf-typegen`.
+ *
+ * Learn more at https://developers.cloudflare.com/durable-objects
+ */
+
+/** A Durable Object's behavior is defined in an exported Javascript class */
+export class MyDurableObject extends DurableObject<Env> {
+	/**
+	 * The constructor is invoked once upon creation of the Durable Object, i.e. the first call to
+	 * 	`DurableObjectStub::get` for a given identifier (no-op constructors can be omitted)
+	 *
+	 * @param ctx - The interface for interacting with Durable Object state
+	 * @param env - The interface to reference bindings declared in wrangler.jsonc
+	 */
+	constructor(ctx: DurableObjectState, env: Env) {
+		super(ctx, env);
+	}
+
+	/**
+	 * The Durable Object exposes an RPC method sayHello which will be invoked when a Durable
+	 *  Object instance receives a request from a Worker via the same method invocation on the stub
+	 *
+	 * @param name - The name provided to a Durable Object instance from a Worker
+	 * @returns The greeting to be sent back to the Worker
+	 */
+	async sayHello(name: string): Promise<string> {
+		return `Hello, ${name}!`;
+	}
+}
+
+export default {
+	/**
+	 * This is the standard fetch handler for a Cloudflare Worker
+	 *
+	 * @param request - The request submitted to the Worker from the client
+	 * @param env - The interface to reference bindings declared in wrangler.jsonc
+	 * @param ctx - The execution context of the Worker
+	 * @returns The response to be sent back to the client
+	 */
+	async fetch(request, env, ctx): Promise<Response> {
+		const url = new URL(request.url);
+
+		if (url.pathname === "/dialogue" && request.method === "POST") {
+			//Logic to build an answer and return to the client
+
+			try {
+				const body = await request.json() as { prompt: string };
+				return new Response(body.prompt);
+			} catch (error) {
+				return new Response("Invalid JSON body", { status: 400 });
+			}
+
+			
+			const stub = env.MY_DURABLE_OBJECT.getByName("foo");
+			const greeting = await stub.sayHello("world");
+			//return new Response(greeting);
+		}
+		if (url.pathname === "/test") {
+			const stub = env.MY_DURABLE_OBJECT.getByName("foo");
+			const greeting = await stub.sayHello("world");
+			return new Response(greeting);
+		}
+		return new Response("Not found", { status: 404 });
+	},
+} satisfies ExportedHandler<Env>;
---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="text-red-700"><p>Hello World</p></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App

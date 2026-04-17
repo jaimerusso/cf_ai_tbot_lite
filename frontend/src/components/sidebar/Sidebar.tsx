aa39703744f719a6f9ecb6f3a4534bdffb5ea049
Jaime Russo <itsjaimerusso@gmail.com>
2026-04-17 18:40:13 +0100

    front: fix button name

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -4,7 +4,7 @@ export default function Sidebar() {
 	return (
 		<div className="w-1/6 bg-dark-gray">
 			<div className="px-2">
-				<Button to="/knowledge" icon="brain" text="New chat" />
+				<Button to="/knowledge" icon="brain" text="Knowledge base" />
 			</div>
 			<div className="px-2">
 				<Button icon="write" text="New chat" />

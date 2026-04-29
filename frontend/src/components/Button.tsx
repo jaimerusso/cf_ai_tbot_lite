0cfe0f720a8dd15f3f1cab5ed675dd2b17901b15
Jaime Russo <itsjaimerusso@gmail.com>
2026-04-29 01:17:27 +0100

    front: removed smooth hover (home screen)

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -17,7 +17,7 @@ export default function Button({
 	return (
 		<Link
 			to={link}
-			className={`py-2 px-3 rounded-lg w-2/6 h-full ${bgVariants[color]} hover:opacity-50 transition flex flex-row justify-center`}
+			className={`py-2 px-3 rounded-lg w-2/6 h-full ${bgVariants[color]} hover:opacity-50 flex flex-row justify-center`}
 		>
 			<p className="text-white font-bold text-xl size-fit transition">
 				{text}
